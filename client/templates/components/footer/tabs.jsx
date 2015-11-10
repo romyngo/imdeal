704bb5adaf7f67fabcf98ce8697187640e45b8b5
romyngo <ngominhquan88@gmail.com>
2015-11-10 21:29:12 +0700

    Moved to dirs

---
+++ client/templates/components/footer/tabs.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+Template.tabs.events({
+
+  'click a.tab-item': function(event) {
+    const target = $(event.target).closest('.tab-item');
+    target.siblings().removeClass('active');
+    target.addClass('active');
+  },
+});
