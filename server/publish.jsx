269b64c7616a025172fbf0c9f138922be656ce8c
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-11-14 13:55:23 +0700

    Fixed up user published fields

--- server/publish.jsx
+++ server/publish.jsx
@@ -1,12 +1,13 @@
 'use strict';
 
-// Auto publish user info
-Meteor.publish(null, function() { return Meteor.users.find(this.userId, {
+// Auto publish user info for current user
+Meteor.publish(null, function() {
+  return Meteor.users.find(this.userId, {
     fields: {
       numFollowers: 1,
       numFollowing: 1,
-      numPosts: 1,
-      listings: 1,
+      numPosts:     1,
+      listings:     1,
     },
   });
 });
