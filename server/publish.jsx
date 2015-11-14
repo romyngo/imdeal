d50986a50d4c4e13854a57a35075e07434da0ce5
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-11-14 10:43:21 +0700

    Overrode default autopublications of user

---
+++ server/publish.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+// Auto publish user info
+Meteor.publish(null, function() { return Meteor.users.find(this.userId, {
+    fields: {
+      numFollowers: 1,
+      numFollowing: 1,
+      numPosts: 1,
+      listings: 1,
+    },
+  });
+});
