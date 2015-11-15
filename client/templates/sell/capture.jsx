9713805d35b7f79cc48d42458dba7299377beaa8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-11-15 13:28:42 +0700

    Reformatted and commented capture helpers/events

--- client/templates/sell/capture.jsx
+++ client/templates/sell/capture.jsx
@@ -4,26 +4,31 @@ Template.capture.onCreated(function() {
   const originalImage = Session.get('originalImage');
 
   if (!originalImage) {
-    uploadPhoto(360, 360, 100);
+    takePhoto(360, 360, 100);
   }
 });
 
+// Add draggable layer ontop of photo
 Template.capture.onRendered(function() {
   $('.crop-layer').draggable({
-    addClass: false,
+    addClass:    false,
     containment: 'parent',
   });
 });
 
-
 Template.capture.events({
+
+  // Callback for clicking Retake button
   'click button.retake-btn': function(event) {
     event.preventDefault();
-    uploadPhoto(360, 360, 100);
+    takePhoto(360, 360, 100);
   },
+
 });
 
 Template.capture.helpers({
+
+  // Returns the image stored in the Session (or default if not present)
   getSessionImage() {
     const originalImage = Session.get('originalImage');
 
@@ -32,14 +37,17 @@ Template.capture.helpers({
 
 });
 
-function uploadPhoto(width, height, quality) {
+// Standard function to take photo and store in session
+function takePhoto(width : int, height : int, quality : int) {
   MeteorCameraUI.getPicture({
-    width: width,
-    height: height,
-    quality: quality,
-  }, function(error, data) {
+    width:    width,
+    height:   height,
+    quality:  quality,
+  }, (error, data) => {
     if (!error) {
+      // Update view to display photo
       $('.image-layer').attr('src', data);
+      // Store photo in Session
       Session.set('originalImage', data);
     }
   });
