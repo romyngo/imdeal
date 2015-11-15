813dc6cf81602d9151a5334487cc2926943b48de
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-11-15 13:07:16 +0700

    Cleaned up settings events and added comments

--- client/templates/profile/settings.jsx
+++ client/templates/profile/settings.jsx
@@ -13,19 +13,22 @@ Template.settings.events({
     AccountsTemplates.logout();
   },
 
-  // Callback for Facebook toggle button
+  // Callback for social network toggle buttons
   'click .toggle-facebook':   (event) => toggleButtonEvent(event, 'facebookShare'),
   'click .toggle-instagram':  (event) => toggleButtonEvent(event, 'instagramShare'),
 
 });
 
-function toggleButtonEvent(event, networkName : string) {
+// For given network button, toggles user sharing settings and changes view to reflect
+function toggleButtonEvent(event, networkName = 'facebookShare' : string) {
   event.preventDefault();
 
+  // Call method to update user sharing settings
   Meteor.call('toggleSharingOption', networkName, !Meteor.user().settings[networkName], (error) => {
     if (error) {
-      return console.error('Could not toggle facebook settings.');
+      return console.error('Could not toggle network sharing settings.');
     }
+    // If everything went fine, change the toggle class so the view updates
     $(event.target).closest('.toggle').toggleClass('active');
   });
 }
