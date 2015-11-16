bd85fa63455babc2979d1d697e2b7022f35b3e94
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-11-16 18:26:17 +0700

    Added chat switching between buying and selling
    - uses Session variable to switch between chats

--- client/templates/messages/chats.jsx
+++ client/templates/messages/chats.jsx
@@ -1,15 +1,28 @@
 'use strict';
 
+Template.chats.onCreated(function() {
+  // Set default option in Session to show buying chats
+  Session.setDefault('showBuyingChats', true);
+});
+
 Template.chats.helpers({
 
-  // Returns array of chats ordered by date and limited by specified amount
+  // Returns array of chats, ordered by date, for either buying or selling chats
   getOrderedChats(limit = 10 : number) {
-    return Chats.find({
-      toUserId: Meteor.userId(),
-    }, {
-      sort: { timestamp: -1 },
-      limit: limit,
-    });
+    if (Session.get('showBuyingChats')) {
+      return getOrderedChats({ toUserId: Meteor.userId() }, limit);
+    } else {
+      return getOrderedChats({ fromUserId: Meteor.userId() }, limit);
+    }
   },
 
 });
+
+// Returns array of chats ordered by date and limited by specified amount
+function getOrderedChats(query : Object, limit : number) {
+  return Chats.find(query, {
+    sort: { timestamp: -1 },
+    limit: limit,
+  });
+}
+
--- client/templates/messages/meta/chatTabs.jsx
+++ client/templates/messages/meta/chatTabs.jsx
@@ -1,10 +1,27 @@
 'use strict';
 
 Template.chatTabs.events({
-  'click .control-item': function(event) {
-    const target = $(event.target);
-    target.siblings().removeClass('active');
-    target.addClass('active');
+
+  // Callback for buying tab
+  'click .control-item.buying-tab': function(event) {
+    switchActiveClass($(event.target));
+
+    // Switch Session variable to allow view to show buying chats
+    Session.set('showBuyingChats', true);
+  },
+
+  // Callback for selling tab
+  'click .control-item.selling-tab': function(event) {
+    switchActiveClass($(event.target));
+
+    // Switch Session variable to allow view to show selling chats
+    Session.set('showBuyingChats', false);
   },
 
 });
+
+// General function to switch active class on buying/selling buttons
+function switchActiveClass(target) {
+  target.siblings().removeClass('active');
+  target.addClass('active');
+}
